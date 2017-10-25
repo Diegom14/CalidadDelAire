4e0026a882c6f369552123127d3d61f7832074d2
mpaulq <31106401+mpaulq@users.noreply.github.com>
2017-10-25 15:20:58 -0300

    Update circuito_completo.ino

--- circuito_completo.ino
+++ circuito_completo.ino
@@ -1,4 +1,4 @@
-#include "DHT.h"
+#include <DHT.h>
 #include <Ethernet.h>
 #include <SPI.h>
 
@@ -6,113 +6,68 @@
 byte mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x51, 0x31}; // Direccion MAC
 byte ip[] = { 192,168,1,122 }; // Direccion IP del Arduino
 byte server[] = { 192,168,1,144 }; // Direccion IP del servidor
-EthernetClient client; 
-float temperatura;
-int analog_pin = 0;
+EthernetClient client;
 
 //Se especifica la entrada digital a la placa Arduino desde el sensor DHT22.
 #define DHTPIN 2
 #define DHTTYPE DHT22
-
 DHT dht(DHTPIN, DHTTYPE);
- 
+
 int samplingTime = 280;
 int deltaTime = 40;
 int sleepTime = 9680;
 
 // Se inicializa la variable "voMeasured" que toma valores entre 0-1023 y que corresponde a la señal analógica,
 // la variable "calcVoltage" es la traducción de la señal analógica al nivel de voltaje. "dustDensity" es el
-// significado de este voltaje en concentración de partículas en mg/m3. 
-
-float calcVoltage = 0;
-float dustDensity = 0;
+// significado de este voltaje en concentración de partículas en mg/m3.
 float dustConc = 0;
 
 // "voltajeSalida" es el nivel de voltaje de salida del circuito con termistor. Se traduce en temperatura "temp"
-float voltajeSalida = 0.0;
-float temp = 0.0;
+float temperatura = 0.0;
 
-String txData = "";
- 
-void setup(){
+void setup(void) {
   Serial.begin(9600);
   Ethernet.begin(mac); // Inicializamos el Ethernet Shield
   delay(1000); // Esperamos 1 segundo de cortesia
   dht.begin();
   pinMode(3,OUTPUT);
 }
-   
-void loop(){
 
+void loop(void) {
   float humedad = dht.readHumidity();
-  float temperatura = dht.readTemperature();
   digitalWrite(3,LOW); // power on the LED
-
   delayMicroseconds(samplingTime);
- 
   delayMicroseconds(deltaTime);
   digitalWrite(3,HIGH); // turn the LED off
   delayMicroseconds(sleepTime);
- 
-  // 0 - 5V mapped to 0 - 1023 integer values
-  // recover voltage
-  calcVoltage = analogRead(A5) * (5.0 / 1023);
- 
-  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-  // Chris Nafis (c) 2012
-  dustDensity = 0.17 * calcVoltage - 0.1;
-  dustConc = (calcVoltage-0.0356)*120000;
 
-  //Se obtiene la salida de tensión, al hacer la siguiente relación.
-  voltajeSalida = 5.0 * analogRead(A0) / 1023;
-  //Para la configuracion con circuito.
-  temp = (4.5355 - voltajeSalida) / 0.048;
- 
-  Serial.print(" - Densidad de Polvo: ");
-  Serial.print(dustDensity);
-  Serial.print(" mg/m3 ");
- 
-  Serial.print(" - Concentración de Polvo: ");
-  Serial.print(dustConc);
-  Serial.print(" ppm ");
- 
-  Serial.print(" - Humedad: ");
-  Serial.print(humedad);
-  Serial.print(" % ");
-
-  Serial.print(" - Temp 1: ");
-  Serial.print(temp);
-  Serial.print(" °C ");
+  dustConc = 5.0*analogRead(A5)/1023.0;
+  dustConc = (dustConc-0.0356)*120000;
   
-  Serial.print(" - Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println(" °C ");
- 
+  temperatura = analogRead(A0);
+  temperatura = 5.0*temperatura/1023.0;
+  temperatura = (4.5355 - temperatura) / 0.048;
+  
   // Proceso de envio de muestras al servidor
-  if (client.connect(server, 80)) {
-    Serial.print("Connected to MySQL server. Sending data...");
-
-    client.print("POST /enviarDatos.php HTTP/1.1\n");
-    client.print("Host: 192.168.1.144\n");
-    client.print("Connection: close\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\n");
-    client.print("Content-Length: ");
-    client.print(txData.length());
-    client.print("\n\n");
-    client.print(txData);
-    Serial.println("Successfull");
-  }
-
-  else{
-    Serial.println("Connection failed");
-    Serial.println();
-
+  Serial.println("Connecting...");
+  if (client.connect(server, 80)>0) {  // Conexion con el servidor
+    client.print("GET /proyectoTics.php?temperatura="); // Enviamos los datos por GET
+    client.print(temperatura);
+    client.print("&humedad=");
+    client.print(humedad);
+    client.print("&polvo=");
+    client.print(dustConc);
+    client.println(" HTTP/1.0");
+    client.println("User-Agent: Arduino 1.0");
+    client.println();
+    Serial.println("Conectado");
+  } else {
+    Serial.println("Fallo en la conexion");
   }
   if (!client.connected()) {
     Serial.println("Disconnected!");
   }
   client.stop();
   client.flush();
- 
-  delay(10000);
+  delay(10000); // Espero 15 seg antes de tomar otra muestra
 }
