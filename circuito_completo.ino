cee2679b9a86d47242f562d788f8d198326d247f
mpaulq <31106401+mpaulq@users.noreply.github.com>
2017-10-27 19:58:46 -0300

    Update circuito_completo.ino

--- circuito_completo.ino
+++ circuito_completo.ino
@@ -1,6 +1,19 @@
+// Se agregan las librerias que se usarán.
 #include <DHT.h>
 #include <Ethernet.h>
 #include <SPI.h>
+
+// Se inicializan las variables
+#define DHTPIN 2
+#define DHTTYPE DHT22
+#define dustPIN 3
+#define dustAnalog 5
+#define termAnalog 0
+
+DHT dht(DHTPIN, DHTTYPE);
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
 
 // Configuracion del Ethernet Shield
 byte mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x51, 0x31}; // Direccion MAC
@@ -9,45 +22,64 @@ byte server[] = { 192,168,1,144 }; // Direccion IP del servidor
 EthernetClient client;
 
 //Se especifica la entrada digital a la placa Arduino desde el sensor DHT22.
-#define DHTPIN 2
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
-
-int samplingTime = 280;
-int deltaTime = 40;
-int sleepTime = 9680;
-
-// Se inicializa la variable "voMeasured" que toma valores entre 0-1023 y que corresponde a la señal analógica,
-// la variable "calcVoltage" es la traducción de la señal analógica al nivel de voltaje. "dustDensity" es el
-// significado de este voltaje en concentración de partículas en mg/m3.
-float dustConc = 0;
-
-// "voltajeSalida" es el nivel de voltaje de salida del circuito con termistor. Se traduce en temperatura "temp"
-float temperatura = 0.0;
 
 void setup(void) {
   Serial.begin(9600);
   Ethernet.begin(mac); // Inicializamos el Ethernet Shield
   delay(1000); // Esperamos 1 segundo de cortesia
   dht.begin();
-  pinMode(3,OUTPUT);
+  pinMode(dustPIN,OUTPUT);
 }
 
 void loop(void) {
-  float humedad = dht.readHumidity();
-  digitalWrite(3,LOW); // power on the LED
-  delayMicroseconds(samplingTime);
-  delayMicroseconds(deltaTime);
-  digitalWrite(3,HIGH); // turn the LED off
-  delayMicroseconds(sleepTime);
-
-  dustConc = 5.0*analogRead(A5)/1023.0;
-  dustConc = (dustConc-0.0356)*120000;
+  double temperatura = Temperatura();
+  double humedad = Humedad();
+  double dustConc = Polvo();
   
-  temperatura = analogRead(A0);
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.print("°C Humedad: ");
+  Serial.print(humedad);
+  Serial.print("% Concentracion de polvo: ");
+  Serial.print(dustConc);
+  Serial.print("ppm");
+  
+  EnviarDatos();
+  delay(10000); // Espero 10 seg antes de tomar otra muestra
+}
+
+double Temperatura() {
+  double temperatura;
+  
+  temperatura = analogRead(termAnalog);
   temperatura = 5.0*temperatura/1023.0;
   temperatura = (4.5355 - temperatura) / 0.048;
   
+  return temperatura;
+}
+
+double Humedad() {
+  double humedad;
+  humedad = dht.readHumidity();
+  return humedad;  
+}
+
+double Polvo() {
+  double polvo;
+  
+  digitalWrite(dustPIN,LOW); // power on the LED
+  delayMicroseconds(samplingTime);
+  delayMicroseconds(deltaTime);
+  digitalWrite(dustPIN,HIGH); // turn the LED off
+  delayMicroseconds(sleepTime);
+  
+  polvo = 5.0*analogRead(A5)/1023.0;
+  polvo = (polvo-0.0356)*120000;
+  
+  return polvo;
+}
+
+void EnviarDatos(server,temperatura,humedad,dustConc) {
   // Proceso de envio de muestras al servidor
   Serial.println("Connecting...");
   if (client.connect(server, 80)>0) {  // Conexion con el servidor
@@ -69,5 +101,4 @@ void loop(void) {
   }
   client.stop();
   client.flush();
-  delay(10000); // Espero 15 seg antes de tomar otra muestra
 }
